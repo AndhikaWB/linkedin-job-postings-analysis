01523e1b3def131c0a93212e09a189bbe4d33cd6
Andhika <17119394+AndhikaWB@users.noreply.github.com>
2023-10-15 22:49:42 +0700

    Completed the setup guide, don't use the arch_package yet

---
+++ _misc/arch_package/apache-hive/apache-hive.sh
@@ -0,0 +1,2 @@
+export HIVE_HOME=/opt/apache-hive
+export PATH=$PATH:$HIVE_HOME/bin
--- _misc/arch_package/hadoop/hadoop.sh
+++ _misc/arch_package/hadoop/hadoop.sh
@@ -3,10 +3,8 @@ export HADOOP_CONF_DIR=/etc/hadoop
 export HADOOP_LOG_DIR=/var/log/hadoop
 export HADOOP_USERNAME=hadoop
 
-#
 # These variables below were personally added by me
 # They don't exist if you download directly from AUR
-#
 
 export HDFS_NAMENODE_USER=hadoop
 export HDFS_DATANODE_USER=hadoop
@@ -14,4 +12,4 @@ export HDFS_SECONDARYNAMENODE_USER=hadoop
 export YARN_NODEMANAGER_USER=hadoop
 export YARN_RESOURCEMANAGER_USER=hadoop
 
-JAVA_HOME=/usr/lib/jvm/java-11-openjdk
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk
